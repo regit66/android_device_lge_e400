265f7e9e3e734765c8136b1d032f9b2b83b8dea9
gellmar <gellmar@yahoo.com>
2013-05-03 11:09:10 +0300

    Revert "Validate bootmode when starting charger" as it brreaks charger-fix
    This reverts commit b338d040936ece024787e6fde6e9cafb08c14f75.

--- e400.mk
+++ e400.mk
@@ -62,9 +62,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/chgchk:system/bin/chgchk
-
 # HW HALS
 PRODUCT_PACKAGES += \
     libgenlock \
